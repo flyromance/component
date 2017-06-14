676e26adcc1cd587cf402917c3718409fae228bf
flyromance <814885004@qq.com>
2017-06-14 22:13:14 +0800

    first commit

---
+++ bin/util.js
@@ -0,0 +1,49 @@
+var glob = require('glob');
+
+// type: js or html
+exports.getEntry = function (ext) {
+    ext = typeof ext === 'string' ? ext : 'js';
+
+    var filenames = glob.sync('./src/*/*.' + ext);
+    var ret = {};
+    // var reg = /\.\/src\/(.*\/.*)\.js/;
+    var reg = new RegExp('\\.\\/src\\/(.*\\/.*)\\.' + ext);
+
+    // item的格式与输入的匹配模式一样
+    filenames.forEach(function (item, index) {
+        var match = null;
+
+        if ((match = reg.exec(item)) && match[1]) {
+            ret[match[1]] = item;
+        }
+    });
+
+    return ret;
+};
+
+
+
+exports.getJsEntry = function () {
+    return getEntry('js');
+};
+
+
+exports.getHtmlPlugins = function (HtmlWebpackPlugin) {
+    var ret = [];
+
+    var filenames = glob.sync('./src/*/*.html');
+    var ret = [];
+
+    // item的格式与输入的匹配模式一样
+    filenames.forEach(function (item, index) {
+        var options = {
+            template: item,
+            inject: false
+        }
+
+        ret.push(new HtmlWebpackPlugin(options));
+    });
+
+
+    return ret;
+};
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var $util = require('./bin/util.js');
+var jsEntrys = $util.getEntry('js');
+// var htmlEntrys = $util.getEntry('html');
+var htmlEntryPlugins = $util.getHtmlPlugins(HtmlWebpackPlugin);
+
+console.log(jsEntrys, htmlEntryPlugins);
+
+module.exports = {
+    entry: jsEntrys,
+    output: {
+        path: path.resolve(__dirname, './dist'), // 2版本必须用绝对路径
+        filename: 'js/[name].[chunkhash].js'
+    },
+    module: {
+    },
+    plugins: [].concat(htmlEntryPlugins)
+
+}
